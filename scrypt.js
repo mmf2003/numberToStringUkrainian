b6bec1adfb5ab125edbaa8d58410ce83cd1b67e7
Chris <mmf2003@ukr.net>
2023-03-05 10:29:18 +0200

    Inialized Object units

--- scrypt.js
+++ scrypt.js
@@ -1,8 +1,27 @@
-function amountInCuirsive(num) {
-    let res = '';
-    let pennies = '';
-    pennies=num.split('.')[1]+' коп.';
-
+let units = {
+    1: 'одна',
+    2: 'дві',
+    3: 'три',
+    4: 'чотири',
+    5: 'пʼять',
+    6: 'шість',
+    7: 'сім',
+    8: 'вісім',
+    9: 'девʼять',
 }
 
-amountInCuirsive('100.00')
+function amountInCuirsive(num) {
+    let res = '';
+    let pennies = num.split('.')[1]+' коп.';
+    let grn = num.split('.')[0].split('');
+
+
+    
+    console.log(grn);
+    console.log(pennies);
+    console.log(units);
+    console.log(units[1]);
+}
+
+amountInCuirsive('111.11')
+
